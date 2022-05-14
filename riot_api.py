5a04c29ad865f88800c53244341558c2e86c3714
Hknots <Hknots99@gmail.com>
2022-05-14 03:39:09 +0200

    Added valid_api func

--- riot_api.py
+++ riot_api.py
@@ -3,36 +3,43 @@ from dotenv import load_dotenv
 import requests
 from database import Database
 from menu import clear_terminal
+import time
 
 db = Database()
 
-def refresh_rank(rowid, ign, server):
+def refresh_ranks():
     load_dotenv()
     api = os.getenv("API")
+    rowid = db.rowids
+    ign = db.igns
+    server = db.servers
 
+    if not valid_api(ign[0], server[0], api): # Checks if API is valid
+        clear_terminal()
+        print("Invalid API\nGet your personal API at https://developer.riotgames.com/ if you want to use this function.")
+        print("If you got an API key, paste it below.")
+
+        new_api = input("API: ") # Lets user paste new API
+        with open('.env', 'w') as env:
+            env.write(f"API={new_api}")
+        return clear_terminal(), print("Returning to main menu..."), time.sleep(1)
+
+    for i in range(len(ign)):
+        user = requests.get(f"https://{server[i]}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{ign[i]}?api_key={api}").json()
+        ranked_stats = requests.get(f"https://{server[i]}.api.riotgames.com/lol/league/v4/entries/by-summoner/{user['id']}?api_key={api}").json()
+
+        if len(ranked_stats) > 0:
+            for i in range(len(ranked_stats)):
+                if ranked_stats[i]['queueType'] == 'RANKED_SOLO_5x5': # If the user has a rank
+                    rank = f"{ranked_stats[i]['tier'].capitalize()} {ranked_stats[i]['rank']}" # f.ex "Diamond I"
+                    db.update("rank", rank, rowid[i]) # Updates the users rank in database
+    
+    return clear_terminal(), print("Rankings updated!"), time.sleep(1)
+
+def valid_api(ign, server, api):
     user = requests.get(f"https://{server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{ign}?api_key={api}").json()
     try:
         if user['status']['status_code'] == 403: # If invalid API
-            clear_terminal()
-            print("You tried refreshing ranks without a valid API")
-            print("Get your personal API from https://developer.riotgames.com/")
-            new_api = input("Paste API if you got one: ")
-            with open('.env', 'w') as env:
-                env.write(f"API={new_api}")
-            return clear_terminal()
-    except KeyError: # If valid API
-        pass
-
-    ranked_stats = requests.get(f"https://{server}.api.riotgames.com/lol/league/v4/entries/by-summoner/{user['id']}?api_key={api}").json()
-
-    if len(ranked_stats) > 0:
-        for i in range(len(ranked_stats)):
-            if ranked_stats[i]['queueType'] == 'RANKED_SOLO_5x5': # If the user has a rank
-                rank = f"{ranked_stats[i]['tier'].capitalize()} {ranked_stats[i]['rank']}"
-                db.update("rank", rank, rowid) # Updates the users rank in database
-                return clear_terminal(), print("Ranked refreshed!"), input("Press ANY button to continue...")
-                
-    # If user has no soloQ Rank
-    rank = "Unranked"
-    db.update("rank", rank, rowid) # Updates the users rank in database
-    return clear_terminal(), print("Ranked refreshed!"), input("Press ANY button to continue...")
+            return False
+    except KeyError: # If api valid
+        return True
